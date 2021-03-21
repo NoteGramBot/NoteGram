116d4a03c19fb5e47d9758d647dfe85a88f46a43
Inigo <igponce@gmail.com>
2021-03-21 09:27:17 +0100

    Lee la configuracion desde un fichero json

--- src/core/core.go
+++ src/core/core.go
@@ -6,20 +6,66 @@
 package core
 
 import (
-   "Notegram/data"
-   "Notegram/tg"
+	"Notegram/data"
+	"Notegram/tg"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
 )
 
 type NotegramError struct {
-   msg string
-}
-func (ee *NotegramError) Error() string {
-    return ee.msg
+	msg string
 }
 
+func (ee *NotegramError) Error() string {
+	return ee.msg
+}
+
+// notegramConfig - los campos tienen que emprezar en mayuscula para exportarse
+// si no, el paquete encoding/json no puede verlos, y no emite ningún error.
+
+type NotegramConfig struct {
+	Secret       string
+	Dbhost       string
+	Dbport       uint
+	Dbuser       string
+	Dbpass       string
+	Dbcollection string
+	Loglevel     string
+}
+
+/*
+ * getConfig()
+ * Obtiene la información de configuracion del bot en forma de map (como el dict de python, o hash de Perl)
+ * - Telegram Secret
+ * - Host/port/pass/index al que escribir en mongodb
+ * - Flag debug
+ */
+
+func GetConfig(jsonfilename string) (NotegramConfig, error) {
+
+	var conf NotegramConfig
+	err := error(nil)
+
+	jsconfig, ee := ioutil.ReadFile(jsonfilename)
+
+	if ee != nil {
+		fmt.Println("No se ha podido leer el fichero config.json\n")
+		return conf, ee
+	}
+
+	err = json.Unmarshal(jsconfig, &conf)
+
+	if err != nil {
+		fmt.Println("error reading json file")
+	}
+
+	fmt.Println(conf)
+
+	return conf, err // the rust borrow checker would love this.
+
+}
 
 func CoreHello() string {
-   return "Hello from Core package AND " + data.DataHello() + " AND " + tg.TgHello()
+	return "Hello from Core package AND " + data.DataHello() + " AND " + tg.TgHello()
 }
-
-
--- src/main.go
+++ src/main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-   "fmt"
-   "Notegram/core"
-   "Notegram/data"
-   telegram "Notegram/tg"
+	"Notegram/core"
+	"Notegram/data"
+	telegram "Notegram/tg"
+	"fmt"
 )
-
 
 func main() {
 
-   fmt.Println("main function")
-   fmt.Println(core.CoreHello())
-   fmt.Println(data.DataHello())
-   fmt.Println(telegram.TgHello())
+	// Bootstrap notegram
+	// Obtiene datos de configuracion del fichero.
+	// OJO: Algunos datos de la configuracion pueden ser sensibles!
+
+	botconfig, err := core.GetConfig("config.json")
+
+	if err != nil {
+		fmt.Println("Configuration summary:\n")
+		fmt.Print(botconfig)
+	}
+
+	fmt.Println("main function")
+	fmt.Println(core.CoreHello())
+	fmt.Println(data.DataHello())
+	fmt.Println(telegram.TgHello())
 
 }
-
-
