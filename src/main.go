90bdac2696e333350c7cef9321ebe9b1ac545f29
Inigo <igponce@gmail.com>
2021-03-25 22:03:35 +0100

    Add cmdline argument to config file (defaults to config.json)

--- src/main.go
+++ src/main.go
@@ -5,15 +5,21 @@ import (
 	"Notegram/data"
 	telegram "Notegram/tg"
 	"fmt"
+        "flag"
 )
 
 func main() {
+
+        // Command line arguments -f config_file_path
+
+        configFilePtr := flag.String("f","config.json", "Path to config file")
+        flag.Parse()
 
 	// Bootstrap notegram
 	// Obtiene datos de configuracion del fichero.
 	// OJO: Algunos datos de la configuracion pueden ser sensibles!
 
-	botconfig, err := core.GetConfig("config.json")
+	botconfig, err := core.GetConfig(*configFilePtr)
 
 	if err != nil {
 		fmt.Println("Configuration summary:")
