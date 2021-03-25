b1e34985fac47a614ffe25eef510bfddf94b23e8
Inigo <igponce@gmail.com>
2021-03-25 22:26:21 +0100

    Implementa bot minimo con funconalidad de ECHO (closes #38).
    Ready to start testing: Codigo que lanza SIGSEGV si el secret de Telegram no es correcto ;-)

--- src/tg/tg.go
+++ src/tg/tg.go
@@ -5,6 +5,11 @@
 
 package tg
 
+import (
+    "fmt"
+    tg_botapi "github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
 // Nota: Habría que llevar un conteo de tipos de error etc...
 type TelegramError struct {
    msg string
@@ -18,4 +23,31 @@ func TgHello() string {
    return "Hello from TG Package"
 }
 
+// tg.Startbot()
+// Ejecuta el bot a partir de la configuracion
 
+func StartBot(secret string) {
+
+   bot, err := tg_botapi.NewBotAPI(secret)
+
+   if err != nil {
+	fmt.Print("Err: " + err.Error())
+   }
+
+   fmt.Println("Bot Name: " + bot.Self.FirstName)
+
+   updates, err := bot.GetUpdatesChan(tg_botapi.NewUpdate(0))
+
+   for uu := range updates {
+
+      if uu.Message != nil {
+		fmt.Println("Update: " + uu.Message.Text)
+		// Devuelve el mensaje (echo server)
+		sendmsg := tg_botapi.NewMessage(uu.Message.Chat.ID, "> "+uu.Message.Text)
+		sendmsg.ParseMode = "markdown"
+		bot.Send(sendmsg)
+	}
+
+   }
+
+}
