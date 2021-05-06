9fddc93322931293464c740b3203efc64d626009
Inigo <igponce@gmail.com>
2021-05-06 23:18:29 +0200

    Inmemory test double limitado a un num de mensajes
    - Pasados `maxmsgs` el test double devuelve un error.
    - Permite testear un error ;-)

--- tg/tg_inmemory.go
+++ tg/tg_inmemory.go
@@ -5,18 +5,28 @@
 
 package tg
 
+import (
+	"fmt"
+	"log"
+)
+
 type InmemoryBotConfig struct {
 	Botconfig BotConfig
-	lastmsg  int
+	lastmsg  int32
+	maxmsg   int32
 }
 
 // tg.Startbot()
 // Ejecuta el bot a partir de la configuracion
 
 
-func NewInmemoryBot() (InmemoryBotConfig,error) {
+func NewInmemoryBot() (*InmemoryBotConfig,error) {
 	bc,err := NewBot()
-	return InmemoryBotConfig{Botconfig:bc}, err
+	return &InmemoryBotConfig{Botconfig:bc, maxmsg: 10}, err
+}
+
+func (bc *InmemoryBotConfig) SetMaxMsgs (maxmsg int32) {
+	bc.maxmsg = maxmsg
 }
 
 func (bc *InmemoryBotConfig) Connect(apikey string) error {
@@ -30,7 +40,7 @@ func (bc *InmemoryBotConfig) Connect(apikey string) error {
 
 func (bc *InmemoryBotConfig) GetMessage() (BotMessage,error) {
 
-	var allmsgs []string = []string{
+	var allmsgs []string = 	[]string{
 		"😃 Lorem ipsum dolor sit amet,",
 		"🧘🏻‍♂️ consectetur adipiscing elit,",
 		"🌍 sed eiusmod tempor incidunt",
@@ -43,28 +53,36 @@ func (bc *InmemoryBotConfig) GetMessage() (BotMessage,error) {
 		"📷 in voluptate velit esse cillum dolore",
 		"🏁 eu fugiat nulla pariatur." }
 
-	bc.lastmsg += 1
-	var currmsg = allmsgs[bc.lastmsg%len(allmsgs)]
+	log.Printf("InmemoryBot - maxmsg=%d , lastmsg = %d", bc.maxmsg, bc.lastmsg)
 
-	return BotMessage{
-		ContentType: "text/plain",
-		Content:     []byte(currmsg),
-		From:        "123",
-		To:          "234" }, nil
+	if (bc.lastmsg < bc.maxmsg) {
+		bc.lastmsg++
+		var currmsg = allmsgs[bc.lastmsg% int32(len(allmsgs))]
+
+		return BotMessage{
+			ContentType: "text/plain",
+			Content:     []byte(currmsg),
+			From:        "123",
+			To:          "234" }, nil
+
+	} else {
+		// disconnected
+		return BotMessage{}, fmt.Errorf("InmemoryBOT ERROR: Superado limite max mensajes enviados (%d)", bc.maxmsg)
+	}
 
 }
 
-func (bc *InmemoryBotConfig) SendMessage(msg BotMessage) error {
+func (bc *InmemoryBotConfig) SendMessage(msg *BotMessage) error {
 	return nil
 }
 
 /* Unimplemented stuff */
 
 
-func (bc InmemoryBotConfig) ListNotes() (msglist []BotMessage, err error) {
+func (bc *InmemoryBotConfig) ListNotes() (msglist []BotMessage, err error) {
 	return nil, nil
 }
 
-func (bc InmemoryBotConfig) Disconnect() error {
+func (bc *InmemoryBotConfig) Disconnect() error {
     return nil
 }
